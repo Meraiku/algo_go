7a7e76679aee12fe17b3a5a009be72692cf29856
Meraiku <meraikuu@gmail.com>
2024-10-10 20:15:10 +0300

    mutex in stack

--- data_structures/stack.go
+++ data_structures/stack.go
@@ -1,30 +1,43 @@
 package datastructures
 
+import "sync"
+
 type Stack[T any] struct {
 	items []T
+	mu    sync.RWMutex
 }
 
 func New[T any]() *Stack[T] {
 	return &Stack[T]{
 		items: []T{},
+		mu:    sync.RWMutex{},
 	}
 }
 
 func (s *Stack[T]) Push(item T) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.items = append(s.items, item)
 }
 
 func (s *Stack[T]) Pop() T {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	if len(s.items) <= 0 {
 		var out T
 		return out
 	}
 	item := s.items[len(s.items)-1]
-	s.items = s.items[: len(s.items)-1 : len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
 	return item
 }
 
 func (s *Stack[T]) Peek() T {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
 	if len(s.items) <= 0 {
 		var out T
 		return out
@@ -33,5 +46,8 @@ func (s *Stack[T]) Peek() T {
 }
 
 func (s *Stack[T]) Size() int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
 	return len(s.items)
 }
